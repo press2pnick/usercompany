7a105d8819d0a9f2bc41524fd310d4ea75e11f3e
shad <nikita.rihter53@gmail.com>
2022-03-29 14:11:45 +0300

    Получение и отображение информации о постах

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const url = 'https://jsonplaceholder.typicode.com/users'
-fetch(url).then(function(response) {
+const urlUser = 'https://jsonplaceholder.typicode.com/users'
+fetch(urlUser).then(function(response) {
     response.json().then(function(data) {
         data.map((value) => { displayUser(value) })
     });
@@ -8,5 +8,19 @@ fetch(url).then(function(response) {
 function displayUser(user) {
     let div = document.createElement('div');
     div.innerHTML = `<div>#${user.id}. ${user.name}</div>`;
+    document.body.append(div);
+}
+
+const urlPosts = 'https://jsonplaceholder.typicode.com/posts'
+fetch(urlPosts).then(function(response) {
+    response.json().then(function(data) {
+        data.map((value) => { displayPosts(value) })
+    });
+});
+
+function displayPosts(post) {
+    let div = document.createElement('div');
+    div.innerHTML = `<div> <p>Title:${post.title}</p> 
+    <p>Body: ${post.body} </p></div>`;
     document.body.append(div);
 }
